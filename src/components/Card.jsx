25c6e237d58c6c8386dc04f45ba0e2155bea4ebe
anshifaanshi <anshifajamsher@gmail.com>
2024-11-07 11:20:23 +0300

    fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,44 +11,39 @@ export const Card = ({ hotels }) => {
   const defaultImage = "https://www.ubereats.com/_static/711d51ca1b458931.webp";
   
   return (
-    <div className="d-flex justify-content-center mb-4">
-      <div className="row">
-        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4"> {/* Use appropriate classes for responsiveness */}
-          <div className="card h-100">
-            <figure className="mb-0 d-flex justify-content-center align-items-center p-3">
-              <img
-                src={hotels?.image ? hotels.image : defaultImage}
-                alt={hotels?.name}
-                className="card-img-top hotelimage rounded-circle"
-              />
-            </figure>
-            <div className="hotelcard card-body text-center">
-              <h5 className="card-title">{hotels?.name}</h5>
-              <p className="card-text">
-                <strong>Address:</strong> {hotels?.address?.street}, {hotels?.address?.city}, {hotels?.address?.state}, {hotels?.address?.country}
-              </p>
-              <p className="card-text">
-                <strong>Rating:</strong> {hotels?.rating} / 5
-              </p>
-              <p className="card-text">
-                <strong>Cuisine:</strong> {hotels?.cuisineType?.join(", ")}
-              </p>
-              <p className="card-text">
-                <strong>Phone:</strong> {hotels?.phone}
-              </p>
-              <p className="card-text">
-                <strong>Email:</strong> {hotels?.email}
-              </p>
-              <div className="d-flex justify-content-center">
-                <Link to={`/hotel/hotelprofile/${hotels?._id}`} className="btn btn-primary">
-                  See More
-                </Link>
+    <div className="container">
+      <div className="row justify-content-center mb-4">
+        {/* Use map to iterate over the data array */}
+        {data?.data?.map((hotel) => (
+          <div key={hotel._id} className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div className="card h-100">
+              <figure className="mb-0 d-flex justify-content-center align-items-center p-3">
+                <img
+                  src={hotel.image || defaultImage}
+                  alt={hotel.name}
+                  className="card-img-top hotelimage rounded-circle"
+                />
+              </figure>
+              <div className="hotelcard card-body text-center">
+                <h5 className="card-title">{hotel.name}</h5>
+                <p className="card-text">
+                  <strong>Phone:</strong> {hotel.phone}
+                </p>
+                <p className="card-text">
+                  <strong>Email:</strong> {hotel.email}
+                </p>
+                <div className="d-flex justify-content-center">
+                  <Link to={`/hotel/hotelprofile/${hotel._id}`} className="btn btn-primary">
+                    See More
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
+         
   );
 };
 
