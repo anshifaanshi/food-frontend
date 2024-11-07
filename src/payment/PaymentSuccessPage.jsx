27652ac63cddff37c2045ab0cabd1069176d1d36
anshifaanshi <anshifajamsher@gmail.com>
2024-11-07 10:50:30 +0300

    fix

--- src/payment/PaymentSuccessPage.jsx
+++ src/payment/PaymentSuccessPage.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import {Link} from 'react-router-dom'
 const PaymentSuccess = () => {
     const location = useLocation();
 
@@ -15,10 +15,17 @@ const PaymentSuccess = () => {
     }, [location]);
 
     return (
-        <div className="payment-success">
-            <h2>Payment Successful!</h2>
-            <p>Thank you for your order. You will receive an email confirmation shortly.</p>
+        <div className="flex items-center justify-center h-screen bg-gray-50">
+        <div className="payment-success bg-white p-10 rounded-lg shadow-lg text-center max-w-sm">
+          <h2 className="text-2xl font-bold text-green-600 mb-4">Payment Successful!</h2>
+          <p className="text-gray-700 mb-6">Thank you for your order. Your payment has been processed successfully.</p>
+          <Link to="/">
+            <button className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 transition duration-300">
+              Back To Home
+            </button>
+          </Link>
         </div>
+      </div>
     );
 };
 
