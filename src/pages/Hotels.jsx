996780c17529b300599383a12cd71156d6cca821
anshifaanshi <anshifajamsher@gmail.com>
2024-11-07 14:35:32 +0300

    fix

--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -1,7 +1,6 @@
-
 import React, { useEffect, useState } from "react";
 import { axiosinstance } from "../config/axiosinstance"; 
-//import { Card } from "../components/Card";
+import { Link } from "react-router-dom"; // Ensure Link is imported
 
 export const Hotel = () => {
     const [data, setData] = useState([]);
@@ -20,7 +19,6 @@ export const Hotel = () => {
         }
     };
 
-    
     useEffect(() => {
         const token = localStorage.getItem("userToken"); 
         console.log("Token found in local storage:", token); 
@@ -31,7 +29,6 @@ export const Hotel = () => {
         fetchHotels();
     }, []);
 
-    
     useEffect(() => {
         console.log("isLoggedIn state updated:", isLoggedIn);
     }, [isLoggedIn]);
@@ -46,35 +43,39 @@ export const Hotel = () => {
 
     return (
         <div className="hotels">
-            
             {isLoggedIn ? (
                 <h1 className="hotelhead">Welcome Back! Explore Our Restaurants</h1>
             ) : (
                 <h1 className="hotelhead">Explore Our Restaurants</h1>
             )}
-<div className="hotel-list">
-  <div className="row">
-    {data.map((hotel) => (
-      <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={hotel?._id}>
-        <div className="card">
-          <img
-            src={hotel?.image || 'default-image.jpg'}
-            alt={hotel?.name}
-            className="card-img-top"
-          />
-          <div className="card-body">
-            <h5 className="card-title">{hotel?.name}</h5>
-            <p className="card-text">{hotel?.description}</p>
-            <p className="card-text">{hotel?.phone}</p>
-            <p className="card-text">{hotel?.email}</p>
-          </div>
-        </div>
-      </div>
-    ))}
-  </div>
-</div>
 
-            
+            <div className="hotel-list">
+                <div className="row">
+                    {data.map((hotel) => (
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={hotel?._id}>
+                            <div className="card">
+                                <img
+                                    src={hotel?.image || 'default-image.jpg'}
+                                    alt={hotel?.name}
+                                    className="card-img-top"
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">{hotel?.name}</h5>
+                                    <p className="card-text">{hotel?.description}</p>
+                                    <p className="card-text">{hotel?.phone}</p>
+                                    <p className="card-text">{hotel?.email}</p>
+                                    <div className="d-flex justify-content-center">
+                                        <Link to={`/hotel/hotelprofile/${hotel?._id}`} className="btn btn-primary">
+                                            See More
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
+
