474f760109c2683af87d1053d4a60337ab108dd0
anshifaanshi <anshifajamsher@gmail.com>
2024-12-07 20:02:53 +0300

    fix

--- src/components/user/UserOrderPage.jsx
+++ src/components/user/UserOrderPage.jsx
@@ -12,7 +12,7 @@ const UserOrdersPage = () => {
     try {
       setLoading(true);
       const response = await axiosinstance({
-        url: "/payment/user/orders",
+        url: "/order/my-orders",
         method: "GET",
       });
       setOrders(response.data.orders);
