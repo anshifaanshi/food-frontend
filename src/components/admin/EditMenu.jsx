09c299a81473d48ae72293f20b445a1da4d527eb
anshifaanshi <anshifajamsher@gmail.com>
2024-12-11 23:01:32 +0530

    fix

--- src/components/admin/EditMenu.jsx
+++ src/components/admin/EditMenu.jsx
@@ -39,7 +39,7 @@ const EditMenu = () => {
     e.preventDefault();
     const updatedFoodItem = foodItems.find(item => item._id === id);
     try {
-      const response = await axiosinstance.put(`/fooditems/update/${id}`, updatedFoodItem);
+      const response = await axiosinstance.put(`/fooditems/${id}`, updatedFoodItem);
       toast.success('Food item updated successfully');
     } catch (error) {
       console.error('Error updating food item:', error);
