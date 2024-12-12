07dc7ad4a0a26e5c00d6ad6ed1661e4d01f5ef03
anshifaanshi <anshifajamsher@gmail.com>
2024-12-12 19:55:52 +0530

    fix

--- src/pages/admin/UserList.jsx
+++ src/pages/admin/UserList.jsx
@@ -79,7 +79,7 @@ const UserList = () => {
                   className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
                   onClick={() => handleDelete(user._id)}
                 >
-                  Delete
+                  Delete User
                 </button>
             </tr>
           ))}
